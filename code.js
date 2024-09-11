c507d78aca8e6d0250c65b2b4eeb34e07331ae2f
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-09-11 15:33:06 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,11 @@
 function insertionSortReverse(array) {
+    for(var i = 1; i < arr.length; i++) {
+    var val = arr[i];
+    var j;
+    for(j = i; j > 0 && arr[j-1] > val; j--) {
+      arr[j] = arr[j-1];
+    }
+    arr[j] = val;
+  }
     return array;
 }
